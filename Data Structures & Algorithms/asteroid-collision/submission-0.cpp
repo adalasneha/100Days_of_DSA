1320f982b995f91467f262e0752f810a2f3a8d02
Sneha Adala <120920554+adalasneha@users.noreply.github.com>
2026-06-20 00:16:48 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        vector<int> st;
+
+        for (int asteroid : asteroids) {
+
+            bool destroyed = false;
+
+            while (!st.empty() && st.back() > 0 && asteroid < 0) {
+
+                if (st.back() < -asteroid) {
+                    // Stack asteroid explodes
+                    st.pop_back();
+                }
+                else if (st.back() == -asteroid) {
+                    // Both explode
+                    st.pop_back();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    // Current asteroid explodes
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push_back(asteroid);
+            }
+        }
+
+        return st;
+    }
+};
